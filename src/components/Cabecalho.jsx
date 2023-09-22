095775857f51ce740400f44016242151e7df8208
davidbryan20 <davidbryan20@icloud.com>
2023-09-22 09:15:14 -0300

    Atualizado as routes

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom";
+import {AiFillHome as H} from "react-icons/ai";
+
 export default function Cabecalho() {
   return (
     <>
@@ -5,13 +8,13 @@ export default function Cabecalho() {
         <h1>Vite + React / Coded By - PF0670</h1>
         <ul>
           <li>
-            <a href="#">Item - 1</a>
+            <Link to="/">HOME - <H/></Link>
           </li>
           <li>
-            <a href="#">Item - 2</a>
+            <Link to="/produtos">PRODUTOS</Link>
           </li>
           <li>
-            <a href="#">Item - 3</a>
+            <Link to="/cadastrar/produto">CADASTRAR PRODUTO</Link>
           </li>
         </ul>
       </header>
